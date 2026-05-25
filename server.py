57b4f6b638b2df5b78fdb3b5c79ace12d61856fe
Xiao-Ling-NCA <chengfuzhao@hotmail.com>
2026-05-24 18:07:45 -0700

    new file:   server.py

---
+++ server.py
@@ -0,0 +1,236 @@
+import random
+
+GAME_END = "GM_END"
+SIZE = 10
+SHIP = ["carrier", "battleship", "cruiser","submarine","destroyer"]
+SHIP_DATA = {
+    "carrier": 5,
+    "battleship": 4,
+    "cruiser": 3,
+    "submarine": 3,
+    "destroyer": 2
+}
+SHIP_REPRESENTATION = {
+    "carrier": 'C',
+    "battleship": 'B',
+    "cruiser": 'Q',
+    "submarine": 'S',
+    "destroyer": 'D'
+}
+UNKNOWN = '~'
+HIT = 'X'
+MISS = 'O'
+
+VERTICAL = "V"
+HORIZONTAL = "H"
+
+ORIENTATIONS = [VERTICAL,HORIZONTAL]
+
+board_header = "X A B C D E F G H I J"
+board_header_to_column = {
+    "A" : 0,
+    "B" : 1,
+    "C" : 2,
+    "D" : 3,
+    "E" : 4,
+    "F" : 5,
+    "G" : 6,
+    "H" : 7,
+    "I" : 8,
+    "J" : 9
+}
+
+player_board = [
+    ['~','~','~','~','~','~','~','~','~','~'],
+    ['~','~','~','~','~','~','~','~','~','~'],
+    ['~','~','~','~','~','~','~','~','~','~'],
+    ['~','~','~','~','~','~','~','~','~','~'],
+    ['~','~','~','~','~','~','~','~','~','~'],
+    ['~','~','~','~','~','~','~','~','~','~'],
+    ['~','~','~','~','~','~','~','~','~','~'],
+    ['~','~','~','~','~','~','~','~','~','~'],
+    ['~','~','~','~','~','~','~','~','~','~'],
+    ['~','~','~','~','~','~','~','~','~','~']
+]
+
+player_guess = [
+    ['~','~','~','~','~','~','~','~','~','~'],
+    ['~','~','~','~','~','~','~','~','~','~'],
+    ['~','~','~','~','~','~','~','~','~','~'],
+    ['~','~','~','~','~','~','~','~','~','~'],
+    ['~','~','~','~','~','~','~','~','~','~'],
+    ['~','~','~','~','~','~','~','~','~','~'],
+    ['~','~','~','~','~','~','~','~','~','~'],
+    ['~','~','~','~','~','~','~','~','~','~'],
+    ['~','~','~','~','~','~','~','~','~','~'],
+    ['~','~','~','~','~','~','~','~','~','~']
+]
+
+server_board = [
+    ['~','~','~','~','~','~','~','~','~','~'],
+    ['~','~','~','~','~','~','~','~','~','~'],
+    ['~','~','~','~','~','~','~','~','~','~'],
+    ['~','~','~','~','~','~','~','~','~','~'],
+    ['~','~','~','~','~','~','~','~','~','~'],
+    ['~','~','~','~','~','~','~','~','~','~'],
+    ['~','~','~','~','~','~','~','~','~','~'],
+    ['~','~','~','~','~','~','~','~','~','~'],
+    ['~','~','~','~','~','~','~','~','~','~'],
+    ['~','~','~','~','~','~','~','~','~','~']
+]
+server_guess = [
+    ['~','~','~','~','~','~','~','~','~','~'],
+    ['~','~','~','~','~','~','~','~','~','~'],
+    ['~','~','~','~','~','~','~','~','~','~'],
+    ['~','~','~','~','~','~','~','~','~','~'],
+    ['~','~','~','~','~','~','~','~','~','~'],
+    ['~','~','~','~','~','~','~','~','~','~'],
+    ['~','~','~','~','~','~','~','~','~','~'],
+    ['~','~','~','~','~','~','~','~','~','~'],
+    ['~','~','~','~','~','~','~','~','~','~'],
+    ['~','~','~','~','~','~','~','~','~','~']
+]
+
+#=====UTILITIES=====
+
+def board_to_string(board):
+    string = ""
+    string = string + board_header + "\n"
+    for r in range(0,SIZE):
+        string = string + str(r) + " "
+        for c in range(0,SIZE):
+            string = string + board[r][c]
+            string = string + " "
+        string = string + "\n"
+    return string
+
+def place_ship(row, col, ship, board, orientation):
+    try:
+        if orientation == VERTICAL:
+                for i in range(0, SHIP_DATA[ship]):
+                    if(board[row+i][col] != UNKNOWN):
+                        return False
+                for i in range(0, SHIP_DATA[ship]):
+                    board[row + i][col] = SHIP_REPRESENTATION[ship]
+                return True
+        elif orientation == HORIZONTAL:
+                for i in range(0, SHIP_DATA[ship]):
+                    if(board[row][col+i] != UNKNOWN):
+                        return False
+                for i in range(0, SHIP_DATA[ship]):
+                    board[row][col+i] = SHIP_REPRESENTATION[ship]
+                return True
+        return False
+    except IndexError:
+        return False
+
+def input_to_coordinate(text):
+    try:
+        column = board_header_to_column[position[0]]
+        row = int(position[1])
+        return (row,column)
+    except:
+        return None
+
+#=====SETUP=====
+
+def player_setup():
+    for i in range(0, len(SHIP)):
+        valid_placement = False
+        while(valid_placement == False):
+            try:
+                print(board_to_string(player_board))
+                print("Setting up for: " + SHIP[i])
+
+                print("Co-ordinates (Letter, Row Number)",end="")
+                result = input_to_coordinate(input())
+                if result != None
+                    row, column = result
+                    print("Orientation [V (Vertical)/ H (Horizontal)]: ", end="")
+                    orientation = input()
+                    valid_placement = place_ship(row, column, SHIP[i], player_board, orientation)
+            except IndexError:
+                print("Try again")
+    return
+
+def server_setup():
+    for i in range(0, len(SHIP)):
+        valid_placement = False
+        while(valid_placement == False):
+            row = random.randint(0,9)
+            col = random.randint(0,9)
+            orient = ORIENTATIONS[random.randint(0,1)]
+
+            valid_placement = place_ship(row, col, SHIP[i],server_board,orient)
+            if valid_placement == False:
+                print("Try again")
+    return
+
+#=====Game Stuff=====
+def check_loss(board):
+    loss = True
+    for r in range(0,SIZE):
+        for c in range(0,SIZE):
+            if board[r][c] != HIT and board[r][c] != MISS and board[r][c] != UNKNOWN:
+                return False
+    return True
+
+def servers_turn(board):
+    row = 0
+    column = 0
+
+    return (row, column)
+
+#=====Runtime=====
+def runtime():
+    player_loss = False
+    server_loss = False
+    
+    while player_loss == False and server_loss == False:
+        print(board_to_string(player_board))
+        print(board_to_string(player_guess))
+
+        #player's turn
+        valid_move = False
+        while valid_move == False:
+            print("Where to hit?")
+            res = input_to_coordinate(input())
+            if res != None:
+                if(player_guess[row][column] != HIT and player_guess[row][column]):
+                    if server_board[row][column] != UNKNOWN:
+                        player_guess[row][column] == HIT
+                        server_board[row][column] == HIT
+                    else:
+                        player_guess[row][column] == MISS
+                        server_board[row][column] == MISS
+                    valid_move = True
+                else:
+                    print("Try again")
+            else:
+                print("Try inputting a valid co-ordinate")
+
+        #check if player has won
+        server_loss = check_loss(server_board)
+
+        if server_loss == False:
+            row, col = servers_turn(server_guess)
+            if player_board[row][column] != UNKNOWN:
+                server_guess[row][column] == HIT
+                player_board[row][column] == HIT
+            else:
+                server_guess[row][column] == MISS
+                player_board[row][column] == MISS
+
+        player_loss = check_loss(player_board)
+
+
+        
+
+def main():
+    player_setup()
+    server_setup()
+
+    runtime()
+
+if __name__ == "__main__":
+    main()
